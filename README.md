75e45b6d364dfe6298a72fee5be38e435c22a93d
Pat Lillis <lillispm@gmail.com>
2016-03-18 15:35:41 -0400

    use Vue

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # kondo
-A browser extension for Chrome based on Marie Kondo's "The life-changing magic of tidying up"
+
+> A browser extension for Chrome based on Marie Kondo's \"The life-changing magic of tidying up\"
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello Vue!'
+    }
+  }
+}
+</script>
+
+<style>
+  body {
+    font-family: Helvetica, sans-serif;
+    background: #B1FFFC;
+  }
+
+  #app {
+    text-align: center;
+    margin-top: 10%;
+  }
+
+  h1 {
+    color:#FF4748;
+    font-size: 64px;
+  }
+</style>
